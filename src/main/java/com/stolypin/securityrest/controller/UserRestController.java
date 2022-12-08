c3cbc198406b9d1b5001f63728ce1a0bc7d7b903
stodmanD <40050958+stodmanD@users.noreply.github.com>
2022-12-08 12:59:02 +0300

    v2.1

--- src/main/java/com/stolypin/securityrest/controller/UserRestController.java
+++ src/main/java/com/stolypin/securityrest/controller/UserRestController.java
@@ -5,6 +5,7 @@ import com.stolypin.securityrest.model.User;
 import com.stolypin.securityrest.services.RoleService;
 import com.stolypin.securityrest.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -24,41 +25,36 @@ public class UserRestController {
     }
 
     @GetMapping
-    public List<User> getAllUsers() {
-        List <User> allUser = userService.getAllUsers();
-        return allUser;
+    public ResponseEntity<List<User>> getAllUsers() {
+        //List <User> allUser = userService.getAllUsers();
+        return ResponseEntity.ok().body(userService.getAllUsers());
     }
 
     @GetMapping("/{id}")
-    public User getUser (@PathVariable Integer id) {
-        User user = userService.getUser(id);
+    public ResponseEntity<User> getUser (@PathVariable Integer id) {
+        //User user = userService.getUser(id);
 
-        return user;
+        return ResponseEntity.ok().body(userService.getUser(id));
     }
 
 @PostMapping
-    public User addNewUser (@RequestBody User user) {
+    public ResponseEntity<User> addNewUser (@RequestBody User user) {
         userService.addUser(user);
-    return user;
+    return ResponseEntity.ok().body(user);
 }
     @PutMapping
-    public User editUser (@RequestBody User user) {
+    public ResponseEntity<User> editUser (@RequestBody User user) {
         userService.saveUser(user);
-        return user;
+        return ResponseEntity.ok().body(user);
     }
     @DeleteMapping("/{id}")
-    public String deleteUser (@PathVariable Integer id) {
+    public ResponseEntity<User> deleteUser (@PathVariable Integer id) {
         User user = userService.getUser(id);
         if (user == null) {
             throw new NoSuchElementException("User by id" + id + "not found");
         }
         userService.deleteUser(id);
-        return "User by " + id + "was deleted";
-    }
-    @GetMapping("/getAuthorizedUser") //при переходе на http://localhost:8080/getAuthorizedUser получаем json авторизованного юзера
-    public User getAuthorizedUser() {
-        User authorizedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return authorizedUser;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
 
